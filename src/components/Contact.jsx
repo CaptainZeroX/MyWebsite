f32a63e537039fc9ca35506edd0a2d0af00a5c7a
CaptainZer0 <mohdlutfialqwasmi2003@gmail.com>
2024-03-06 17:27:25 +0300

    done with basics

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,20 +1,107 @@
-import React from "react";
+import React, { useState } from "react";
 import { SectionWrapper } from "../hoc";
 import { styles } from "../styles";
 import { motion } from "framer-motion";
-import { textVariant } from "../utils/motion";
+import { slideIn, textVariant } from "../utils/motion";
 import PhoneCanvas from "./canvas/Phone";
+import { useRef } from "react";
+import emailjs from '@emailjs/browser';
 const Contact = () => {
+  const formRef = useRef();
+  const[form,setForm] = useState({
+    name:"",
+    email:"",
+    message:"",
+  })
+  const [loading, setLoading] = useState(false);
+  const handleChange =(e)=>{
+    const {name, value} = e.target;
+    setForm({...form, [name]: value})
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    emailjs.send ('service_v88fm5y', 'template_0qfhl4f',
+    {from_name:form.name, to_name:"Moh'd Lutfi", from_email:form.email, to_email: 'mohdlutfialqwasmi2003@gmail.com', message:form.message}, 
+    'fnah8kGN5BslgeXaA')
+    .then((result) => {
+      console.log(result.text);
+      setLoading(false);
+      alert('THank you for your message, I will get back to you soon!');
+      setForm({
+        name:"",
+        email:"",
+        message:"",
+      })
+    }, (error) => {
+      console.log(error.text);
+      setLoading(false);
+      alert('Sorry, something went wrong, please try again later');
+    });
+
+  }
+
   return (
     <>
       <motion.div variants={textVariant()}>
         <p className={styles.sectionHeadText}>CONTACT</p>
-        <PhoneCanvas />
-
-        <p> Hekllo</p>
-        {/*{<"ABOUT ME/>"} */}
       </motion.div>
-    </>
+    
+   <div className="xl:mt-4 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
+    
+     {/*<motion.div variants={textVariant()}>*/}  
+   
+      <motion.div variants={slideIn('left', "tween", 0.2, 1)} className=" flex-[0.75] p-8 rounded-2xl">
+        <form ref={formRef}
+        onSubmit={handleSubmit}
+        className="flex flex-col gap-8">
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your Name</span>
+            <input
+              type="text"
+              name="name"
+              value={form.name}
+              onChange={handleChange}
+              placeholder="whats your name?"
+              className="bg-white py-4 px-6 placeholder:text-secondary text-black rounded-lg outlined-none border-none font-medium"
+            />
+            </label>
+            <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your Email</span>
+            <input
+              type="email"
+              name="email"
+              value={form.email}
+              onChange={handleChange}
+              placeholder="whats your email?"
+              className="bg-white py-4 px-6 placeholder:text-secondary text-black rounded-lg outlined-none border-none font-medium"
+            />
+            </label>
+            <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your Message</span>
+            <textarea
+              rows="7"
+              name="message"
+              value={form.message}
+              onChange={handleChange}
+              placeholder="Type your message here"
+              className="bg-white py-4 px-6 placeholder:text-secondary text-black rounded-lg outlined-none border-none font-medium"
+            />
+            </label>
+            <button type="submit" className="py-3 px-8 outline-none w-fit text-white ffont-bold shadow-md shadow-primary rounded-xl">
+              {loading ? "Sending..." : "Send"}
+
+            </button>
+
+        </form>
+        </motion.div>
+        <motion.div variants={slideIn('right', "tween", 0.2, 1)} className="xl:flex-1 xl:h-auto md: h-[550px] h-[350px]">
+          <PhoneCanvas/>
+        </motion.div>
+       
+     
+</div>
+</>
   );
 };
 
--- src/components/canvas/Phone.jsx
+++ src/components/canvas/Phone.jsx
@@ -2,35 +2,61 @@ import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
+import { PerspectiveCamera } from "@react-three/drei";
+import { EffectComposer,Pixelation, DepthOfField, Bloom, Noise, Vignette, SSAO} from "@react-three/postprocessing";
+import { BlurPass, Resizer, KernelSize } from 'postprocessing';
+import { SelectiveBloom } from "@react-three/postprocessing";
+
+
 
 const Phone = () => {
   const phone = useGLTF("./contact_m/scene.gltf");
 
   return (
-    <primitive object={phone.scene} scale={2} position-y={0} rotation-y={0} />
+    <mesh>
+      <hemisphereLight intensity={1} color={[1.3,1,0.4]}/>
+    <primitive object={phone.scene}  scale={2.2}
+       position={[0, -10, 0]}
+        rotation={[-0, -0, -0]} />
+    </mesh>
   );
 };
 
+/**
+ * Renders a canvas with a phone model.
+ * @returns {JSX.Element} The rendered PhoneCanvas component.
+ */
 const PhoneCanvas = () => {
   return (
     <Canvas
-      shadows
-      frameloop="demand"
-      dpr={[1, 2]}
-      gl={{ preserveDrawingBuffer: true }}
-      camera={{
-        fov: 45,
-        near: 0.1,
-        far: 200,
-        position: [-4, 3, 6],
-      }}
+    frameloop="demand"
+    shadows
+    camera={{ fov: 75, near:0.1, far: 200, position:[-4,5,20]}}
+    
+    gl={{ preserveDrawingBuffer: true }} // properly to render model
     >
+       
+       
       <Suspense fallback={<CanvasLoader />}>
+      <EffectComposer smaa>
+       <Pixelation
+        granularity={1} // pixel granularity
+        />
+        <Bloom
+          intensity={1.4}
+          luminanceThreshold={0.1}
+          luminanceSmoothing={0.015}
+          mipmapBlur={false}
+        />
+   
+    <SSAO />
+      </EffectComposer>
         <OrbitControls
           autoRotate
           enableZoom={false}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
+          target={[0, 0, 0]}
         />
         <Phone />
 
